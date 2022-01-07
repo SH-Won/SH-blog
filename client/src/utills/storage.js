9402df5694b407aeaac853b0f1511792c82b34a5
SH-Won <1317good@naver.com>
2022-01-08 00:35:48 +0900

    use localStorage

---
+++ client/src/utills/storage.js
@@ -0,0 +1,27 @@
+const storage = localStorage;
+
+export const getItem = (key) =>{
+    try{
+        const value = storage.getItem(key);
+        return value ? JSON.parse(value) : [];
+    }
+    catch{
+        return [];
+    }
+} 
+export const setItem = (key,value) =>{
+    try{
+        storage.setItem(key,JSON.stringify(value));
+    }
+    catch{
+
+    }
+}
+export const removeItem = (key) =>{
+    try{
+        storage.removeItem(key)
+    }
+    catch{
+
+    }
+}
