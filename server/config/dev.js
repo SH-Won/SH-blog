f2a28dad5f8f056d3af11aba6f38c5333cc1ee02
SH-Won <1317good@naver.com>
2022-05-27 01:07:21 +0900

    remove git cache

--- server/config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    mongoURI:'mongodb+srv://db1212:wlfkfqud1@db-1212.zaaec.mongodb.net/main?retryWrites=true&w=majority',
-    cloud_name:'dhjegsbqv',
-    api_key:'849556649919811',
-    api_secret:'BWYDsBBg6F4FP07xYqs1lORdWOk'
-}
